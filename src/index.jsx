38f09814753e5f72cd57dafc6363b8503065cf49
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-05-18 14:04:27 +0200

    add container to main view

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MainView } from './components/main-view/main-view';
+import { Container } from 'react-bootstrap';
 
 import './index.scss';
 
 class MyFlixApplication extends React.Component {
   render() {
     return (
-    <MainView />
+      <Container>
+        <MainView />
+      </Container>
+    
     );
   }
 }
