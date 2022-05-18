2d9072ea0577f639364ceb502777bceaa73ca49d
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-05-18 14:04:05 +0200

    add style to login view

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+
+export function LoginView(props) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    props.onLoggedIn(username);
+  };
+
+  return (
+    <Form>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control
+          type="text"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" onClick={handleSubmit}>
+        Submit
+      </Button>
+    </Form>
+  );
+}
