8f4b25a72ec5ce0c0d4dfdefd663d141631cab62
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-05-19 17:42:29 +0200

    refactor login view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Container, Card, CardGroup } from "react-bootstrap";
+
+import "./login-view.scss"
 
 export function LoginView(props) {
   const [username, setUsername] = useState("");
@@ -14,18 +17,34 @@ export function LoginView(props) {
 
   return (
     <Form>
-      <Form.Group controlId="formUsername">
+      <h2>LOGIN PAGE</h2>
+      <Form.Group className="mb-3" controlId="formUsername">
         <Form.Label>Username:</Form.Label>
-        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+        <Form.Control
+          type="text"
+          onChange={(e) => setUsername(e.target.value)}
+        />
       </Form.Group>
 
       <Form.Group controlId="formPassword">
         <Form.Label>Password:</Form.Label>
-        <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+        <Form.Control className="mb-3"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </Form.Group>
       <Button variant="primary" type="submit" onClick={handleSubmit}>
-        Submit
+        SIGN IN
       </Button>
+      <p className="text-center">
+        Don't have an account? <span className="btn btn-primary ml-3 mt-3">SIGN UP</span>
+      </p>
     </Form>
   );
 }
+
+LoginView.propTypes = {
+  name: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  onLoggedIn: PropTypes.func.isRequired,
+};
