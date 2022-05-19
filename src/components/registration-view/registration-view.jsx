fb951467bc687065ec582c71567be233d6a05cc8
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-05-19 11:02:45 +0200

    add style to registration view

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
+import {
+  Form,
+  Button,
+  Card,
+  CardGroup,
+  Container,
+  Col,
+  Row,
+} from "react-bootstrap";
 
 export function RegistartionView(props) {
-
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
@@ -10,38 +18,69 @@ export function RegistartionView(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username,password,email,birthday);
+    console.log(username, password, email, birthday);
     props.onRegisteredIn(false);
-  }
+  };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </label>
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      <label>
-        Email: 
-        <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
-      </label>
-      <label>
-        Birthday: 
-        <input type="date" value={birthday} onChange={(e)=>setBirthday(e.target.value)}/>
-      </label>
-      <button type='submit' onClick={handleSubmit}>Register</button>
-    </form>
-  )
-}
+    <Container>
+      <Row>
+        <Col>
+          <CardGroup>
+            <Card>
+              <Card.Body>
+              <Card.Title>Please Register</Card.Title>
+              <Form>
+                <Form.Group>
+                  <Form.Label>Username:</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    placeholder="Enter a username"
+                  />
+                </Form.Group>
 
+                <Form.Group>
+                  <Form.Label>Password:</Form.Label>
+                  <Form.Control
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    minLength="8"
+                    placeholder="Password must be 8 or more characters"
+                  />
+                </Form.Group>
+
+                <Form.Group>
+                  <Form.Label>Email:</Form.Label>
+                  <Form.Control
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="user@gmail.com"
+                  />
+                </Form.Group>
+
+                <Form.Group>
+                  <Form.Label>Birthday:</Form.Label>
+                  Birthday:
+                  <Form.Control
+                    type="date"
+                    value={birthday}
+                    onChange={(e) => setBirthday(e.target.value)}
+                  />
+                </Form.Group>
+                <Button variant="primary" type="submit" onClick={handleSubmit}>
+                  Register
+                </Button>
+              </Form>
+              </Card.Body>
+            </Card>
+          </CardGroup>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
