74951957a759087b69aa6974888764594a9459f0
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-05-20 16:20:41 +0200

    update styles

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 import { Form, Button, Container, Card, CardGroup } from "react-bootstrap";
 
-import "./login-view.scss"
+import "./login-view.scss";
 
 export function LoginView(props) {
   const [username, setUsername] = useState("");
@@ -16,30 +16,34 @@ export function LoginView(props) {
   };
 
   return (
-    <Form>
-      <h2>LOGIN PAGE</h2>
-      <Form.Group className="mb-3" controlId="formUsername">
-        <Form.Label>Username:</Form.Label>
-        <Form.Control
-          type="text"
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </Form.Group>
+    <div className="color-overlay d-flex justify-content-center align-items-center">
+      <Form className="rounded p-4 p-sm-3">
+        <h2>LOGIN PAGE</h2>
+        <Form.Group className="mb-3" controlId="formUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control
+            type="text"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </Form.Group>
 
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password:</Form.Label>
-        <Form.Control className="mb-3"
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </Form.Group>
-      <Button variant="primary" type="submit" onClick={handleSubmit}>
-        SIGN IN
-      </Button>
-      <p className="text-center">
-        Don't have an account? <span className="btn btn-primary ml-3 mt-3">SIGN UP</span>
-      </p>
-    </Form>
+        <Form.Group controlId="formPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+            className="mb-3"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit" onClick={handleSubmit}>
+          SIGN IN
+        </Button>
+        <p className="text-center">
+          Don't have an account?{" "}
+          <span className="btn btn-primary ml-3 mt-3">SIGN UP</span>
+        </p>
+      </Form>
+    </div>
   );
 }
 
