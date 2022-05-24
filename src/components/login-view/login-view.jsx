ddc3940590a2762eb3c44cb3c5083c6fd6667603
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-05-24 19:09:21 +0200

    refactor code login-view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -9,20 +9,47 @@ export function LoginView(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  const [usernameErr, setUsernameErr] = useState("");
+  const [passwordErr, setPasswordErr] = useState("");
+
+  // validate user inputs
+  const validate = () => {
+    let isReq = true;
+    if (!username) {
+      setUsernameErr("Username Required");
+      isReq = false;
+    } else if (username.length < 2) {
+      setUsernameErr("Username must be 2 characters long");
+      isReq = false;
+    }
+    if (!password) {
+      setPasswordErr("Password Required");
+      isReq = false;
+    } else if (password.length < 4) {
+      setPassword("Password must be 4 characters long");
+      isReq = false;
+    }
+    return isReq;
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    // send a request to server for authentication
-    axios.post('https://movie-api-1112.herokuapp.com/login', {
-      Username: username,
-      Password: password
-    })
-      .then(response => {
-        const data = response.data;
-        props.onLoggedIn(data);
-      })
-      .catch(e => {
-        console.log('no such user')
-      });
+    const isReq = validate();
+    if (isReq) {
+      // send a request to server for authentication
+      axios
+        .post("https://movie-api-1112.herokuapp.com/login", {
+          Username: username,
+          Password: password,
+        })
+        .then((response) => {
+          const data = response.data;
+          props.onLoggedIn(data);
+        })
+        .catch((e) => {
+          console.log("no such user");
+        });
+    }
   };
 
   return (
@@ -34,9 +61,9 @@ export function LoginView(props) {
           <Form.Control
             type="text"
             placeholder="Enter username"
-            value={ username }
+            value={username}
             onChange={(e) => setUsername(e.target.value)}
-          />
+          /> {usernameErr && <p>{ usernameErr }</p>}
         </Form.Group>
 
         <Form.Group controlId="formPassword">
@@ -45,9 +72,9 @@ export function LoginView(props) {
             className="mb-3"
             type="password"
             placeholder="Enter password"
-            value={ password }
+            value={password}
             onChange={(e) => setPassword(e.target.value)}
-          />
+          /> { passwordErr && <p>{ passwordErr }</p>}
         </Form.Group>
         <Button variant="primary" type="submit" onClick={handleSubmit}>
           SIGN IN
